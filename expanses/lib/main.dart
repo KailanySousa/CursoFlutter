85a315dae481e6dc3aad4b4ed9863c2056e945ff
KailanySousa <kailany-sousa2010@hotmail.com>
2021-12-28 20:01:57 -0300

    criando projeto

---
+++ expanses/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ExpansesApp(),
+    ),
+  );
+}
+
+class ExpansesApp extends StatelessWidget {
+  const ExpansesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Center(
+        child: Text("Versão Inicial"),
+      ),
+    );
+  }
+}
