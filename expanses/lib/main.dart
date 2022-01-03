bf25e877a96143b36c49bba33f5b689f9d7d9d62
KailanySousa <kailany-sousa2010@hotmail.com>
2022-01-03 19:51:27 -0300

    feat: modo paisagem #03

--- expanses/lib/main.dart
+++ expanses/lib/main.dart
@@ -99,6 +99,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
     final appBar = AppBar(
       textTheme: ThemeData.light().textTheme.copyWith(
             headline6: TextStyle(
@@ -128,31 +130,33 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Exibir Fráfico'),
-                Switch(
-                  value: _showChart,
-                  onChanged: (value) {
-                    setState(
-                      () {
-                        _showChart = value;
-                      },
-                    );
-                  },
-                ),
-              ],
-            ),
-            _showChart
-                ? Container(
-                    height: availableHeight * 0.3,
-                    child: Chart(_recentTransactions),
-                  )
-                : Container(
-                    height: availableHeight * 0.7,
-                    child: TransactionList(_transactions, removeTransaction),
+            if (isLandscape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Exibir Fráfico'),
+                  Switch(
+                    value: _showChart,
+                    onChanged: (value) {
+                      setState(
+                        () {
+                          _showChart = value;
+                        },
+                      );
+                    },
                   ),
+                ],
+              ),
+            if (_showChart || !isLandscape)
+              Container(
+                height: availableHeight * (!isLandscape ? 0.7 : 0.3),
+                child: Chart(_recentTransactions),
+              ),
+            if (!_showChart || !isLandscape)
+              Container(
+                height: availableHeight * 0.7,
+                child: TransactionList(_transactions, removeTransaction),
+              ),
           ],
         ),
       ),
